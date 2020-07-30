68fcc361fdbb3ca4d92dedb9351b9539ac188616
Michael Simons <michael@simons.ac>
2020-07-30 12:10:59 +0200

    GH-6 - Add checkstyle and fix violations.
    Copied over from neo4j-migrations and others, also applied sorting the
    pom.
    
    This closes #6.

--- src/main/java/org/neo4j/junit/jupiter/causal_cluster/DefaultNeo4jServer.java
+++ src/main/java/org/neo4j/junit/jupiter/causal_cluster/DefaultNeo4jServer.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2019-2020 "Neo4j,"
- * Neo4j Sweden AB [http://neo4j.com]
+ * Neo4j Sweden AB [https://neo4j.com]
  *
  * This file is part of Neo4j.
  *
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,7 +38,7 @@ final class DefaultNeo4jServer implements Neo4jServer, AutoCloseable {
 	 */
 	private final URI externalURI;
 
-	public DefaultNeo4jServer(Neo4jContainer<?> container, URI externalURI) {
+	DefaultNeo4jServer(Neo4jContainer<?> container, URI externalURI) {
 		this.container = container;
 		this.externalURI = externalURI;
 	}
